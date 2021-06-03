59706d046a958a9f2a9c158405d48eda480aae23
eun9999 <dmsrud990323@naver.com>
2021-06-03 16:13:13 +0900

    push notification 마무리

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {Button, KeyboardAvoidingView, FlatList, View, Text, TouchableOpacity, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Button,
+  KeyboardAvoidingView,
+  FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
-    showNotification,
-    handleScheduleNotification,
-    handleCancel
+  createChannel,
+  showNotification,
+  handleScheduleNotification,
+  handleCancel,
 } from './src/notification';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import SQLite from 'react-native-sqlite-2';
 
 import style from '../components/styles';
+import {create} from 'eslint/lib/rules/*';
 
 var db = '';
 db = SQLite.openDatabase('pubKey.db', '1.0', '', 1);
@@ -25,7 +36,7 @@ function HomeScreen({navigation}) {
   useEffect(() => {
     db.transaction(tx => {
       tx.executeSql('SELECT * FROM pubKey', [], function (tx, res) {
-        var temp = []
+        var temp = [];
         for (let i = 0; i < res.rows.length; ++i) {
           temp.push(res.rows.item(i).key);
           setAddressList(temp);
@@ -33,62 +44,61 @@ function HomeScreen({navigation}) {
       });
     });
     console.log(addressList);
-    const addressApiCall = async (addr) => {
-      try{
-        const response = await axios.get('https://api.blockcypher.com/v1/btc/main/addrs/'+addr);
+    const addressApiCall = async addr => {
+      try {
+        const response = await axios.get(
+          'https://api.blockcypher.com/v1/btc/main/addrs/' + addr,
+        );
         console.log(response.data);
         // console.log(response.data.address + '\'s info load success');
-      } catch(e) {
-        console.error(addr + '\'s info load failed');
+      } catch (e) {
+        console.error(addr + "'s info load failed");
       }
     };
     for (let i = 0; i < addressList.length; i++) {
       addressApiCall(addressList[i]);
     }
-  }, [])
+  }, []);
 
   return (
-      <SafeAreaView style={style.root}>
-        <View style={style.Header}>
-          <Text style={{color: 'white'}}>Watch Bot</Text>
-        </View>
-        <View style={style.Body}>
-          <Text>body</Text>
-          <KeyboardAvoidingView style={{flex: 1}}>
-                  <FlatList
-
-                  />
-                </KeyboardAvoidingView>
-        </View>
-        <View style={styles.container}>
-              <Text>Push Notification</Text>
-              <TouchableOpacity activeOpacity={0,6}
-                onPress={()=>showNotification('hello', 'message')}>
-              <View style={styles.button}>
-                  <Text style={styles.button}>
-                    Click me to get notification
-                  </Text>
-              </View>
-              </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    );
+    <SafeAreaView style={style.root}>
+      <View style={style.Header}>
+        <Text style={{color: 'white'}}>Watch Bot</Text>
+      </View>
+      <View style={style.Body}>
+        <Text>body</Text>
+        <KeyboardAvoidingView style={{flex: 1}}>
+          <FlatList />
+        </KeyboardAvoidingView>
+      </View>
+      <View style={styles.container}>
+        <Text>Push Notification</Text>
+        <TouchableOpacity
+          activeOpacity={(0, 6)}
+          onPress={() => showNotification('watchbot', 'hello', 'message')}>
+          <View style={styles.button}>
+            <Text style={styles.button}>Click me to get notification</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        padding: 16,
-        backgroundColor: 'blue',
-        borderRadius: 24,
-        marginTop: 16,
-    },
-    buttonTitle : {
-        color: 'white',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 16,
+    backgroundColor: 'blue',
+    borderRadius: 24,
+    marginTop: 16,
+  },
+  buttonTitle: {
+    color: 'white',
+  },
 });
--- src/screens/src/notification.android.js
+++ src/screens/src/notification.android.js
@@ -1,22 +1,38 @@
-import PushNotification from 'react-native-push-notification'
+import PushNotification from 'react-native-push-notification';
 
-const showNotification = (title, message)=>{
-    PushNotification.localNotification({
-        title: title,
-        message: message,
-    });
+const createChannel = () => {
+  PushNotification.createChannel(
+    {
+      channelId: 'watchbot',
+      channelName: 'com.temp',
+    },
+    created => console.log(`createChannel returned '${created}'`),
+  );
 };
 
-const handleScheduleNotification = (title, message)=>{
-    PushNotification.localNotificationSchedule({
-        title: title,
-        message: message,
-        date: new Date(Date.now() + 5 * 1000),
-    });
+const showNotification = (id, title, message) => {
+  PushNotification.localNotification({
+    channelId: id,
+    title: title,
+    message: message,
+  });
+};
+
+const handleScheduleNotification = (title, message) => {
+  PushNotification.localNotificationSchedule({
+    title: title,
+    message: message,
+    date: new Date(Date.now() + 5 * 1000),
+  });
 };
 
 const handleCancel = () => {
-    PushNotification.cancelAllLocalNotifications();
+  PushNotification.cancelAllLocalNotifications();
 };
 
-export {showNotification, handleScheduleNotification, handleCancel};
+export {
+  createChannel,
+  showNotification,
+  handleCancel,
+  handleScheduleNotification,
+};
