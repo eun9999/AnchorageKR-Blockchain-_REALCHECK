3020171a365a2510b3e38d79148af1b83a1e6d86
이봉훈 <drok02@naver.com>
2021-06-28 01:52:56 +0900

    로그인 merge v.0

--- App.js
+++ App.js
@@ -3,14 +3,53 @@ import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
+import 'react-native-gesture-handler';
 import HomeScreen from './src/screens/HomeScreen';
 import InsertPubKey from './src/screens/InsertPubKey';
 import SettingScreen from './src/screens/SettingScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
 import SplashScreen from 'react-native-splash-screen';
 
+
+
+
+
+// Import Screens
+
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
+
+
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
+  return (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={RegisterScreen}
+        options={{
+          title: 'Register', //Set Header Title
+          headerStyle: {
+            backgroundColor: '#307ecc', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   setTimeout(() => {
@@ -18,7 +57,34 @@ export default function App() {
   }, 10);
   return (
     <NavigationContainer>
-      <Tab.Navigator
+      <Stack.Navigator initialRouteName="Auth">
+        {/* SplashScreen which will come once for 5 Seconds
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          // Hiding header for Splash Screen
+          options={{headerShown: false}}
+        /> */}
+
+        {/* Auth Navigator: Include Login and Signup */}
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+          
+        <Stack.Screen
+          name="DrawerNavigationRoutes"
+          component={DrawerNavigationRoutes}
+          // Hiding header for Navigation Drawer
+          options={{headerShown: false}}
+        />
+ 
+   
+
+
+     
+      
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
             let iconName;
@@ -32,7 +98,7 @@ export default function App() {
             return <Icon name={iconName} size={size} color={'white'} />;
           },
         })}
-        tabBarOptions={{
+        {/* tabBarOptions={{
           style: {
             backgroundColor: 'black',
             borderColor: 'black',
@@ -40,11 +106,12 @@ export default function App() {
           activeTintColor: 'white',
           inactiveTintColor: 'white',
           keyboardHidesTabBar: true,
-        }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="PubKey" component={InsertPubKey} />
-        <Tab.Screen name="Settings" component={SettingScreen} />
-      </Tab.Navigator>
+        }}> */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="PubKey" component={InsertPubKey} />
+        <Stack.Screen name="Settings" component={SettingScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
+  
   );
 }
